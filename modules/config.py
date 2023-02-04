226c81ed6ebba82aa50ac6c11916eae11bb1785a
hvnlygod <122079677+hvnlygod@users.noreply.github.com>
2023-02-04 13:38:35 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -18,13 +18,13 @@ Config = {
     "chromedriver_path": r"path/to/chromedriver",  # if windows 'chromedriver.exe'
     "use_custom_proxy" : False, #default is False change to True to use a file containing multiple proxies of yours.
     "use_local_ip_address" : True, #default is False chnage to True to user your computers ip directly.
-    "amount_of_account": 1, #amount of account you want to create make sure it doesnt exceed 50 for better performance
+    "amount_of_account": 10, #amount of account you want to create make sure it doesnt exceed 50 for better performance
     "amount_per_proxy": 1, #this would be amont of account used if you have a you are using multiple proxies
     "proxy_file_path" : ASSET_DIR + "/proxies.txt",
     "email_domain": "gmail.com",
-    "country": "it",
+    "country": "ir",
     "activation_email_addr": "xxxxxxxxxxxxxxxx",
-    "activation_email_pass": "xxxxxxxxxxxxxxxx",
+    "activation_email_pass": "UNLIMITED",
     "activation_email_serv": "xxxxxxxxxxxxxxxx",
     "activation_email_spor": 993,
 }
